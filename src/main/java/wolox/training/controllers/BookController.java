eb7f7604fd7afc2e6a92531c25dcebe70da2ba1d
Darvand <david.montoya@wolox.co>
2020-04-23 11:09:51 -0500

    Format code

--- src/main/java/wolox/training/controllers/BookController.java
+++ src/main/java/wolox/training/controllers/BookController.java
@@ -8,10 +8,13 @@ import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class BookController {
-  @GetMapping("/greeting")
-  public String greeting(@RequestParam(name = "name", required = false, defaultValue = "World") String name, Model model) {
-    model.addAttribute("name", name);
-    return "greeting";
-  }
+
+    @GetMapping("/greeting")
+    public String greeting(
+            @RequestParam(name = "name", required = false, defaultValue = "World") String name,
+            Model model) {
+        model.addAttribute("name", name);
+        return "greeting";
+    }
 }
 
