54b688eb7954dc3e1f3093d9faa3421fa677ee9c
Darvand <david.montoya@wolox.co>
2020-04-27 09:29:34 -0500

    Reformat all project code

--- src/main/java/wolox/training/exceptions/ExceptionResponse.java
+++ src/main/java/wolox/training/exceptions/ExceptionResponse.java
@@ -4,26 +4,26 @@ import java.util.Date;
 
 public class ExceptionResponse {
 
-  private String message;
-  private String origin;
-  private Date timestamp;
+    private String message;
+    private String origin;
+    private Date timestamp;
 
-  public ExceptionResponse(String message, String origin) {
-    super();
-    this.timestamp = new Date();
-    this.message = message;
-    this.origin = origin;
-  }
+    public ExceptionResponse(String message, String origin) {
+        super();
+        this.timestamp = new Date();
+        this.message = message;
+        this.origin = origin;
+    }
 
-  public String getMessage() {
-    return message;
-  }
+    public String getMessage() {
+        return message;
+    }
 
-  public String getOrigin() {
-    return origin;
-  }
+    public String getOrigin() {
+        return origin;
+    }
 
-  public Date getTimestamp() {
-    return timestamp;
-  }
+    public Date getTimestamp() {
+        return timestamp;
+    }
 }
--- src/main/java/wolox/training/exceptions/books/BookIdMissMatchException.java
+++ src/main/java/wolox/training/exceptions/books/BookIdMissMatchException.java
@@ -1,7 +1,8 @@
 package wolox.training.exceptions.books;
 
 public class BookIdMissMatchException extends RuntimeException {
-  public BookIdMissMatchException() {
-    super("Parameter ID miss match with body Book ID");
-  }
+
+    public BookIdMissMatchException() {
+        super("Parameter ID miss match with body Book ID");
+    }
 }
